c0103070b7bdd126a02f0080ed2f08cbce49dbba
farhandardiri <faran.f4124n@gmail.com>
2023-08-17 22:23:32 +0700

    build 17 8 23

--- dist/assets/theme-559ee4c6.js
+++
@@ -1,1 +0,0 @@
-import{x as e}from"./index-f1170237.js";const a=e("setTheme",{state:()=>({themes:localStorage.getItem("pageTheme")?JSON.parse(localStorage.getItem("pageTheme")):null})});export{a as u};
