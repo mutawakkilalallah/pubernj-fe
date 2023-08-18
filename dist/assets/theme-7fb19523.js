94a6338e940bb22018dcb74e1647beb95ca74f60
farhandardiri <faran.f4124n@gmail.com>
2023-08-18 23:32:24 +0700

    bulild 18 8 23

---
+++ dist/assets/theme-7fb19523.js
@@ -0,0 +1,1 @@
+import{x as e}from"./index-e66b72c4.js";const a=e("setTheme",{state:()=>({themes:localStorage.getItem("pageTheme")?JSON.parse(localStorage.getItem("pageTheme")):null})});export{a as u};
